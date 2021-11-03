e9f58f1181052c31bc830e1a36ba1e009b322e31
all user <all@gmail.com>
2021-11-03 15:50:59 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <h1 className="bg-yellow-700">its contains all dependencies</h1>
-  );
+  return <h1 className="bg-yellow-700">this is second</h1>;
 }
 
 export default App;
